634334cbfae3416c99ef0a06ceb63bf0e5a2531f
Andrew M <liquidsun@gmail.com>
2013-03-06 12:08:04 -0600

    add incremental message processing

--- poly1305-donna.h
+++ poly1305-donna.h
@@ -3,6 +3,12 @@
 
 #include <stddef.h>
 
-void poly1305_auth(unsigned char mac[16], const unsigned char *m, size_t len, const unsigned char key[32]);
+typedef unsigned char poly1305_state[512];
+
+void poly1305_init(poly1305_state *state, const unsigned char key[32]);
+void poly1305_update(poly1305_state *state, const unsigned char *m, size_t bytes);
+void poly1305_finish(poly1305_state *state, unsigned char mac[16]);
+void poly1305_auth(unsigned char mac[16], const unsigned char *m, size_t inlen, const unsigned char key[32]);
 
 #endif /* POLY1305_DONNA_H */
+
